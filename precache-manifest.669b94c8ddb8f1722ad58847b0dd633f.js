c9322c9786f98eac2b64a338bcae2df60f55939f
bliid <gh.bliid@gmail.com>
2019-09-13 13:01:46 +0100

    Updates

---
+++ precache-manifest.669b94c8ddb8f1722ad58847b0dd633f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14aa06b26d74f8df774bf71669bf737a",
+    "url": "/dyn-credit-card/index.html"
+  },
+  {
+    "revision": "9e1fededb1b069df7f05",
+    "url": "/dyn-credit-card/static/css/main.f7f139d6.chunk.css"
+  },
+  {
+    "revision": "54ea0b359ece360fb2a2",
+    "url": "/dyn-credit-card/static/js/2.4952cce3.chunk.js"
+  },
+  {
+    "revision": "9e1fededb1b069df7f05",
+    "url": "/dyn-credit-card/static/js/main.21aec453.chunk.js"
+  },
+  {
+    "revision": "6f044155c5cc68c8b101",
+    "url": "/dyn-credit-card/static/js/runtime~main.a2c87b64.js"
+  }
+]);
